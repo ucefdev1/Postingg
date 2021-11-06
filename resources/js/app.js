2b8c8b43820ca7fd803e8dedf2d50cd1114b0e84
Ucef Dev <contact.ucefdev@gmail.com>
2021-11-06 13:56:01 +0100

    fix components loader problem

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+
+import { fromPairs } from 'lodash';
+import Vue from 'vue'
+
+import postinngIndex from './components/Postinng/Index.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {postinngIndex}
+  
+})
---
+++ resources/js/components/Postinng/Index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>title</th>
+                    <th>text</th>
+                    <th>date</th>
+                    <th>action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>text</td>
+                    <td>something</td>
+                    <td>11-11-2020</td>
+                    <td></td>
+                </tr>
+
+                 <tr>
+                    <td>text</td>
+                    <td>something</td>
+                    <td>11-11-2020</td>
+                    <td></td>
+                </tr>
+
+                 <tr>
+                    <td>text</td>
+                    <td>something</td>
+                    <td>11-11-2020</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default{
+    
+}
+
+</script>
