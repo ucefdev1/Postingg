a30f399ca8b04ec2b3775eb9016b6319a34c3862
Ucef Dev <contact.ucefdev@gmail.com>
2021-11-11 01:16:41 +0100

    prevent double submit

--- resources/js/components/Postinng/Create.vue
+++ resources/js/components/Postinng/Create.vue
@@ -4,17 +4,29 @@
           Post title:
           <br/>
           <input type="text" v-model="fields.title" id="" class="form-control">
+          <div class="alert-danger p-1" v-if="errors && errors.title">
+              {{errors.title[0]}}
+          </div>
           <br/>
           Post Text:
           <br/>
           <textarea class="form-control" v-model="fields.post_text" rows="10"></textarea>
+                 <div class="alert-danger p-1" v-if="errors && errors.post_text">
+              {{errors.post_text[0]}}
+          </div>
           <br/>
           Category:
           <select  class="form-control" v-model="fields.category_id">
               <option  v-for="category in categories" :value="category.id">{{category.name}}</option>
           </select>
+             <div class="alert-danger p-1" v-if="errors && errors.category_id">
+              {{errors.category_id[0]}}
+          </div>
           <br/>
-          <input type="submit" value="Save post" class="btn btn-primary">
+          <input type="submit" 
+          :value="form_submitting ? 'saving post...' : 'save post' " 
+          :disabled="form_submitting"
+          class="btn btn-primary">
       </form>
       
     </div>
@@ -30,7 +42,10 @@ export default {
                 title:'',
                 post_text:'',
                 category_id: ''
-            }
+            },
+            errors: {
+            },
+            form_submitting: false
         }
     },
    mounted(){
@@ -41,11 +56,20 @@ export default {
    }, 
    methods: {
        submit_form(){
-           console.log('test');
+           //console.log('test');
+           this.form_submitting = true;
               axios.post('api/posts',this.fields)
 				.then(response => {
 					this.$router.push('/');
-				});
+                               this.form_submitting = false;
+
+				}).catch(error => {
+                    if(error.response.status === 422){
+                        this.errors = error.response.data.errors;
+                                   this.form_submitting = false;
+
+                    }
+                });
        }
    }
 
